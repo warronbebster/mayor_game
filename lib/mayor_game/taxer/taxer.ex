cd4c27777ecc0fdea8370036ed20c73a17afab8f
Barron Webster <visualwebster@gmail.com>
2021-02-21 17:04:29 -0500

    calculate best possible job

--- lib/mayor_game/taxer/taxer.ex
+++ lib/mayor_game/taxer/taxer.ex
@@ -1,6 +1,7 @@
 defmodule MayorGame.Taxer do
   use GenServer, restart: :permanent
   alias MayorGame.City
+  alias MayorGame.City.Details
 
   def val(pid) do
     GenServer.call(pid, :val)
@@ -35,37 +36,26 @@ defmodule MayorGame.Taxer do
     {:reply, val, val}
   end
 
-  def init(initial_val) do
+  def init(_initial_val) do
     # send message :tax to self process after 5000ms
     Process.send_after(self(), :tax, 5000)
+    world = MayorGame.Repo.get!(MayorGame.City.World, 1)
 
     # returns ok tuple when u start
-    {:ok, initial_val}
+    {:ok, world.day}
   end
 
   # when tick is sent
   def handle_info(:tax, val) do
-    buildables = MayorGame.City.Details.detail_buildables()
     cities = City.list_cities_preload()
-
     world = MayorGame.Repo.get!(MayorGame.City.World, 1)
+
     # increment day
     City.update_world(world, %{day: world.day + 1})
     IO.puts("day: " <> to_string(world.day))
 
     for city <- cities do
       operating_cost = calculate_daily_cost(city)
-
-      # for each building in housing
-      # eventually i could use Stream instead of Enum if cities is loooooong
-      available_housing =
-        Enum.reduce(buildables.housing, 0, fn {building_type, building_options}, acc ->
-          # get fits, multiply by number of buildings
-          acc + building_options.fits * Map.get(city.detail, building_type)
-        end)
-
-      # returns a map of %{0 => #, 0 => #, etc}
-      available_jobs = calculate_jobs(city)
 
       tax_income = calculate_taxes(city)
 
@@ -79,6 +69,7 @@ defmodule MayorGame.Taxer do
       if List.first(city.citizens) != nil do
         updated_city_treasury =
           if city.detail.city_treasury + tax_income - operating_cost < 0 do
+            # maybe some other consequences here
             0
           else
             city.detail.city_treasury + tax_income - operating_cost
@@ -118,34 +109,92 @@ defmodule MayorGame.Taxer do
     # would this be where you can also pubsub over to users that are connected?
     # return noreply and val
     # increment val
-    {:noreply, val + 1}
+    {:noreply, world.day + 1}
   end
 
   def calculate_taxes(%MayorGame.City.Info{} = city) do
+    city_preloaded = preload_city_check(city)
     # if there is a citizen
-    if List.first(city.citizens) != nil do
-      Enum.reduce(city.citizens, 0, fn citizen, acc ->
-        City.update_citizens(citizen, %{age: citizen.age + 1})
+    available_housing = calculate_housing(city_preloaded)
+    # returns a map of %{0 => #, 0 => #, etc}
+    available_jobs = calculate_jobs(city_preloaded)
 
-        # function to spawn children
+    if List.first(city_preloaded.citizens) != nil do
+      results =
+        Enum.reduce(
+          city_preloaded.citizens,
+          %{jobs: available_jobs, tax: 0, housing: available_housing},
+          fn citizen, acc ->
+            City.update_citizens(citizen, %{age: citizen.age + 1})
 
-        # function to look for other cities
+            # function to spawn children
 
-        # kill citizen if over this age
-        # should add if there are no houses + citizen has no other city option
-        if citizen.age > 36500, do: City.delete_citizens(citizen)
+            # function to look for other cities
 
-        1 + citizen.job + acc
-      end)
+            # if there are NO jobs for citizen, returns -1.
+            best_possible_job =
+              if citizen.education > 0 do
+                # [3,2,1,0]
+                job_levels_to_check = Enum.reverse(0..citizen.education)
+                IO.inspect(job_levels_to_check)
+
+                Enum.reduce_while(job_levels_to_check, citizen.education, fn level_to_check,
+                                                                             job_acc ->
+                  if acc.jobs[level_to_check] > 0,
+                    do: {:halt, job_acc},
+                    else: {:cont, job_acc - 1}
+                end)
+              else
+                if acc.jobs[0] > 0, do: 0, else: -1
+              end
+
+            IO.puts("citizen edu level: " <> to_string(citizen.education))
+            IO.puts("best_possible_job: " <> to_string(best_possible_job))
+            job_gap = citizen.education - best_possible_job
+
+            # function to look for education if have money
+
+            # need deeper logic here to check if there are jobs available at the level
+            # basically… "check if there is job at my max level"
+            # if there is, great
+            # if not, add variable to search other cities
+            # right now you can end up with negative jobs in a job_level
+
+            updated_jobs =
+              if best_possible_job >= 0,
+                do: acc.jobs |> Map.put(citizen.education, acc.jobs[best_possible_job] - 1),
+                else: acc.jobs
+
+            # kill citizen if over this age
+            # should add if there are no houses + citizen has no other city option
+            # also kill based on cars
+            if citizen.age > 36500, do: City.delete_citizens(citizen)
+
+            %{jobs: updated_jobs, tax: 1 + citizen.job + acc.tax, housing: acc.housing - 1}
+          end
+        )
+
+      # return just the tax number
+      results.tax
     else
       0
     end
   end
 
+  def calculate_housing(%MayorGame.City.Info{} = city) do
+    city_preloaded = preload_city_check(city)
+
+    Enum.reduce(Details.buildables().housing, 0, fn {building_type, building_options}, acc ->
+      # get fits, multiply by number of buildings
+      acc + building_options.fits * Map.get(city_preloaded.detail, building_type)
+    end)
+  end
+
   def calculate_jobs(%MayorGame.City.Info{} = city) do
+    city_preloaded = preload_city_check(city)
     empty_jobs_map = %{0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0}
 
-    Enum.reduce(MayorGame.City.Details.detail_buildables(), empty_jobs_map, fn category, acc ->
+    Enum.reduce(Details.buildables(), empty_jobs_map, fn category, acc ->
       {categoryName, buildings} = category
 
       if categoryName != :housing && categoryName != :civic do
@@ -155,7 +204,7 @@ defmodule MayorGame.Taxer do
            jobs +
              Enum.reduce(buildings, 0, fn {building_type, building_options}, acc2 ->
                if building_options.job_level == job_level do
-                 acc2 + building_options.jobs * Map.get(city.detail, building_type)
+                 acc2 + building_options.jobs * Map.get(city_preloaded.detail, building_type)
                else
                  acc2
                end
@@ -169,14 +218,23 @@ defmodule MayorGame.Taxer do
   end
 
   def calculate_daily_cost(%MayorGame.City.Info{} = city) do
+    city_preloaded = preload_city_check(city)
     # for each element in the details struct options
-    Enum.reduce(MayorGame.City.Details.detail_buildables(), 0, fn category, acc ->
+    Enum.reduce(MayorGame.City.Details.buildables(), 0, fn category, acc ->
       {_categoryName, buildings} = category
 
       acc +
         Enum.reduce(buildings, 0, fn {building_type, building_options}, acc2 ->
-          acc2 + building_options.ongoing_price * Map.get(city.detail, building_type)
+          acc2 + building_options.ongoing_price * Map.get(city_preloaded.detail, building_type)
         end)
     end)
+  end
+
+  def preload_city_check(%MayorGame.City.Info{} = city) do
+    if !Ecto.assoc_loaded?(city.detail) do
+      city |> MayorGame.Repo.preload([:citizens, :user, :detail])
+    else
+      city
+    end
   end
 end
