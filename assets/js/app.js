762bc29c0f473685f538fb20ad418494050c0dd6
Barron Webster <visualwebster@gmail.com>
2021-08-07 20:48:26 -0400

    hide upgrades behind mayor check

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,11 +16,12 @@ import "../css/app.scss";
 import "phoenix_html";
 
 import { Socket } from "phoenix";
-import LiveSocket from "phoenix_live_view";
+import { LiveSocket } from "phoenix_live_view";
 
-// let liveSocket = new LiveSocket("/live", Socket);
+
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+// let liveSocket = new LiveSocket("/live", Socket);
 let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken } });
 
 liveSocket.connect();
