1d6ef323eb0ae86a38da5b035cd030d847d03173
Alex Kotliarskyi <alex.frantic@gmail.com>
2022-11-01 18:57:10 -0700

    Downgrade Elixir to see if it runs on Render

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MayorGame.MixProject do
     [
       app: :mayor_game,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
