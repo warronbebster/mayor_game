ca3f3eceed8f479d9fde248beddd807f9c5e28b4
Barron Webster <visualwebster@gmail.com>
2023-01-31 10:41:56 -0500

    si

--- priv/repo/migrations/20230131034418_add_protection_to_cities.exs
+++
@@ -1,9 +0,0 @@
-defmodule MayorGame.Repo.Migrations.AddProtectionToCities do
-  use Ecto.Migration
-
-  def change do
-    alter table(:cities) do
-      add :shields, :integer, default: 0
-    end
-  end
-end
