4a0490de65e83e54fb6f3865f0bcb86dee63c80d
Barron Webster <visualwebster@gmail.com>
2023-02-01 13:12:00 -0500

    nuked old ones

--- priv/repo/migrations/20230131034418_add_protection_to_cities.exs
+++
@@ -1,9 +0,0 @@
-defmodule MayorGame.Repo.Migrations.AddSulfurGoldUranium do
-  use Ecto.Migration
-
-  def change do
-    alter table(:cities) do
-      add :shields, :integer, default: 0
-    end
-  end
-end
