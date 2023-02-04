0debcc5428c8cabe9b4c0cbc0858230bf72dbfcd
Barron Webster <visualwebster@gmail.com>
2023-02-03 20:31:47 -0500

    update

--- config/runtime.exs
+++ config/runtime.exs
@@ -43,9 +43,9 @@ if config_env() == :prod do
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "25"),
     timeout: 20_000,
-    socket_options: maybe_ipv6
+    socket_options: maybe_ipv6,
     queue_target: 10_000,
-    queue_interval: 30_000,
+    queue_interval: 30_000
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
