fc8683e8814c73e56eddab246a4c09dbeed67de8
Barron Webster <visualwebster@gmail.com>
2023-02-14 23:02:40 -0500

    send resources

--- lib/mayor_game_web/views/city_view.ex
+++ lib/mayor_game_web/views/city_view.ex
@@ -1,3 +1,12 @@
 defmodule MayorGameWeb.CityView do
   use MayorGameWeb, :view
+
+  # attr :field, Phoenix.HTML.FormField
+  # attr :rest, include: ~w(type)
+
+  def input(assigns) do
+    ~H"""
+    <input id={@field.id} name={@field.name} value={@field.value} {@rest} />
+    """
+  end
 end
