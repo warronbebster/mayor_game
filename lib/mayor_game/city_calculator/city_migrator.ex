650f90f99b0c8a5f6211ca7f24b11ed1a17f6c6f
Barron Webster <visualwebster@gmail.com>
2023-02-18 08:51:44 -0500

    less home-city-advantage

--- lib/mayor_game/city_calculator/city_migrator.ex
+++ lib/mayor_game/city_calculator/city_migrator.ex
@@ -124,7 +124,7 @@ defmodule MayorGame.CityMigrator do
     health_min = Enum.min(Enum.map(leftovers, fn city -> city.health end))
     health_spread = health_max - health_min
 
-    home_city_advantage = 0.1
+    home_city_advantage = 0.05
 
     slotted_cities_by_id =
       leftovers
