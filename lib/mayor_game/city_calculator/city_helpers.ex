d96f61dbae0712846525b28ca2658b99bcae9fd0
Barron Webster <visualwebster@gmail.com>
2023-02-18 14:43:24 -0500

    births adj

--- lib/mayor_game/city_calculator/city_helpers.ex
+++ lib/mayor_game/city_calculator/city_helpers.ex
@@ -385,7 +385,7 @@ defmodule MayorGame.CityHelpers do
           # i can just calculate this globally. doesn't really matter on a per-citizen basis
           will_citizen_reproduce =
             citizen.age > 15 and acc.housing_left > 1 &&
-              :rand.uniform(citizen_count) < max(results.health / 100, 5)
+              :rand.uniform(citizen_count) < max(results.health / 10, 10)
 
           housing_taken = if will_citizen_reproduce, do: 2, else: 1
 
