4771cb74a7025b9108ed007f80f9b0bea952806b
Barron Webster <visualwebster@gmail.com>
2023-03-07 23:46:03 -0500

    way less births and constrain buildables count

--- priv/repo/migrations/20230308034110_constraint_to_positive.exs
+++ priv/repo/migrations/20230308034110_constraint_to_positive.exs
@@ -2,6 +2,6 @@ defmodule MayorGame.Repo.Migrations.ConstraintToPositive do
   use Ecto.Migration
 
   def change do
-    create constraint("cities", :treasury_must_be_positive, check: "treasury > 0")
+    create constraint("cities", :treasury_must_be_positive, check: "treasury >= 0")
   end
 end
